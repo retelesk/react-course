eb8c784a3f79bf4bedcb54d10aff420eb9fbd237
retelesk <tranhongquan.jb@gmail.com>
2023-05-14 00:07:34 +0700

    a

--- src/components/Expenses/ExpenseContainer.js
+++ src/components/Expenses/ExpenseContainer.js
@@ -18,26 +18,14 @@ const ExpenseContainer = function (props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          title={propArr[0].title}
-          amount={propArr[0].amount}
-          date={propArr[0].date}
-        />
-        <ExpenseItem
-          title={propArr[1].title}
-          amount={propArr[1].amount}
-          date={propArr[1].date}
-        />
-        <ExpenseItem
-          title={propArr[2].title}
-          amount={propArr[2].amount}
-          date={propArr[2].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={propArr[3].title}
-          amount={propArr[3].amount}
-          date={propArr[3].date}
-        />
+        {propArr.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
