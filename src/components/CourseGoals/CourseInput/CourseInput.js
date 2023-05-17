a020e910f8d89f0a33f5201775988bdcd6f385a2
retelesk <tranhongquan.jb@gmail.com>
2023-05-17 22:53:57 +0700

    update

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,8 +1,6 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React, {useState} from "react";
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
-
+import styles from "./CourseInput.module.css"; /*
 const FormControl = styled.div`
   margin: 0.5rem 0;
 
@@ -29,6 +27,35 @@ const FormControl = styled.div`
     border-color: #8b005d;
   }
 `;
+*/
+/*
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${(props) => (props.invalid ? "red" : "black")}
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+    background: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")}
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+`;
+*/
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
   const [isValid, setIsValid] = useState(true);
@@ -50,7 +77,9 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
+      <div
+        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
         <input
           style={{
@@ -60,7 +89,7 @@ const CourseInput = (props) => {
           type="text"
           onChange={goalInputChangeHandler}
         />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,6 +1,5 @@
-import "./Button.css";
-import styled from "styled-components";
-
+import styles from "./Button.module.css";
+/*
 const Button = styled.button`
   width: 100%;
   font: inherit;
@@ -25,14 +24,13 @@ const Button = styled.button`
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
   }
 `;
+ */
 
-/*
 const Button = (props) => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
 };
-*/
 export default Button;
